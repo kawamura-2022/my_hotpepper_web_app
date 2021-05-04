8898350609007273f4f58c62255d0fa6e7f05d7a
kawamura-2022 <job7310info@gmail.com>
2021-05-05 01:50:07 +0900

    トップに説明文表示

--- docker-react-app/my_hotpepper_web_app/src/App.js
+++ docker-react-app/my_hotpepper_web_app/src/App.js
@@ -3,15 +3,11 @@ import SearchForm from './Form/SearchForm'
 
 import MyGeolocated from './API/my_Geolocated' //HACK: 混乱を避けるために絶対パスで指定できるようにする
 
-
 function App() {
   return (
-    <div className="App">      
-  
+    <div className="App">
       <MyGeolocated />
       <SearchForm />      
-  
-      
     </div>
   );
 }
