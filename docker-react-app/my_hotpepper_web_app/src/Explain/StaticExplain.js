efd8330d4b2174584fdf432963be92cb1316d880
kawamura-2022 <job7310info@gmail.com>
2021-05-05 16:48:25 +0900

    不要ファイル削除 簡易仕様書追加

--- docker-react-app/my_hotpepper_web_app/src/Explain/StaticExplain.js
+++ docker-react-app/my_hotpepper_web_app/src/Explain/StaticExplain.js
@@ -11,7 +11,8 @@ class StaticExplain extends React.Component {
 
         return (
             <div class="row">
-                <div class="col-md-6 col-md-offset-3" style={style}>            
+                <br></br>
+                <div class="col-md-6 col-md-offset-3" style={style}>
                     このサイトでは，現在地付近の飲食店を探す事ができます．<br></br>
                     現在地を取得するボタンをクリックし，検索条件を設定すれば，<font color="#FF97C2">会員登録なし</font>て使用できます．                
                 </div>
